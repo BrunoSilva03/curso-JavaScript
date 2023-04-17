84cf5d9e277223017a866892ffce5bbf0cd4e9e3
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-04-17 14:59:48 -0300

    funções e vetorial recursivo

---
+++ aula16/funcao01.js
@@ -0,0 +1,17 @@
+function parimp(n) {
+    if(n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+console.log(parimp(223));
+
+
+//OU
+
+/*
+let resultado = parimp(6);
+console.log(resultado);
+*/
