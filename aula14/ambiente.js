58c6b390f0351b751fbeb27417ae074d13a4dd6e
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-04-17 12:05:00 -0300

    for

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+/*
+var c = 1;
+while(c <=5) {
+    console.log(c);
+    c++;
+}
+
+do {
+    console.log(c);
+    c++;
+}while(c <=5);
+
+*/
+
+
+for(c = 1; c <= 5; c++){  //Todas as 3 formas de estruturas de repetição tem o mesmo resultado nesse exemplo.
+
+    console.log(c);
+}
