aa1704f08adfcea1ae4ccfe2da33e693293fc6b5
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-04-21 11:10:30 -0300

    fim do programa

--- aula14ex/exercicio16/script.js
+++ aula14ex/exercicio16/script.js
@@ -5,13 +5,17 @@ function contar() {
     let res = document.getElementById('res');
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Faltam dados para contar.'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando... ';
+        res.innerHTML = 'Contando: <br> ';
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
-
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
 
             for (let c = i; c <= f; c += p) {
