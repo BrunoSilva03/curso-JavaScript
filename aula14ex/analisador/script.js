5fa32e50fa40d22d3b0ef973d9c436b9c539d01f
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-04-24 13:29:48 -0300

    aquecendo

--- aula14ex/analisador/script.js
+++ aula14ex/analisador/script.js
@@ -1,10 +1,10 @@
 
 
 function adicionar() {
-    let number = document.getElementById('txtnum');
+    let number = window.document.getElementById('txtnum');
     let num = Number(number);
 
     if (num <= 0 || num > 100) {
         window.alert('Número inválido');
     }
-}
+} //só pra falar que fiz alguma coisa
